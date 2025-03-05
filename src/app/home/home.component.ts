06598b72d180e56462414b0c1799e827e0cdac6a
Kingsley Leung <kingsleyleung2003@outlook.com>
2025-03-06 12:30:33 +1300

    Initialise Housing Location Interface Connect it to the Home Component.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 
+import { HousingLocation } from '../housinglocation';
+
 @Component({
   selector: 'app-home',
   imports: [HousingLocationComponent],
@@ -8,5 +10,16 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test City',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
