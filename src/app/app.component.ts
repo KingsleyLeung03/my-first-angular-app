d1b8f62deb3b26bf484dc6b27ec3de7443c59ee3
Kingsley Leung <kingsleyleung2003@outlook.com>
2025-03-07 15:02:24 +1300

    Implement Details Page Including the routing.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
